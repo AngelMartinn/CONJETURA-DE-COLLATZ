48b8e45d42e697e7add1b9f8376d86fe12a21054
AngelMartin <83008534+AngelMartinn@users.noreply.github.com>
2021-10-26 17:34:55 +0200

    Creado La interfac Usuario
    solo recoje los datos inicales para trabajr con ellos mas adelante

---  Teorema-3x-1/src/teorema/pkg3x/pkg1/Principal.java
+++  Teorema-3x-1/src/teorema/pkg3x/pkg1/Principal.java
@@ -1,7 +1,29 @@
 package teorema.pkg3x.pkg1;
+import java.util.HashMap;
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-         
+         InterfazUsuario();
     }// main()
+    
+    private static void InterfazUsuario(){
+        int Metodo;
+        int NumHilos;
+        BigInteger Inicio; 
+        BigInteger Fin;
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Bienvenido al programa calculadora de casos del Teorema 3x-1");
+        System.out.println("Que cantiad de hilos quiere utilizar");
+        NumHilos = teclado.nextInt();teclado.nextLine();
+        System.out.println("Que metodo quieres utilizar:\n \t DeUnoEnUno-1 \n\t SubIntervalos-2");
+        Metodo = teclado.nextInt(); teclado.nextLine();
+        System.out.println("Dime el inicio de las señillas a calcular");
+        Inicio = new BigInteger(teclado.nextLine());
+        System.out.println("Por ultimo dime el final de las semillas a calcular");
+        Fin= new BigInteger(teclado.nextLine());
+        
+    }
 }// Principal
