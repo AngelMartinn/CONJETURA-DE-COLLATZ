f71d33febb127a8d3b3a90fc1590c83a83162557
Jorge Salmon <kokesalmon@gmail.com>
2021-11-01 20:56:06 +0100

    commit avanzo
    falta que los rangos no cojan el fin del anterior como inicio

--- Teorema 3x+1/src/teorema/pkg3x/pkg1/DatosRangos.java
+++ Teorema 3x+1/src/teorema/pkg3x/pkg1/DatosRangos.java
@@ -1,8 +1,6 @@
 package teorema.pkg3x.pkg1;
 
 import java.math.BigInteger;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class DatosRangos {
     private BigInteger inicioRango;
@@ -11,12 +9,15 @@ public class DatosRangos {
     private BigInteger numMax;
     private BigInteger reparto;
     private int despertador=0;
+    private int contadorHilos = 0;
+    private BigInteger finINICIAL;
 
     public DatosRangos(BigInteger inicioRango, BigInteger finRango, int numHilos) {
         this.inicioRango = inicioRango;
         this.finRango = finRango;
+        finINICIAL=finRango;
         this.numHilos = numHilos;
-        reparto = finRango.divide(BigInteger.valueOf(numHilos));
+        reparto = (finRango.subtract(inicioRango)).divide(BigInteger.valueOf(numHilos));
     }// DatosRangos()
 
     public BigInteger getNumMax() {
@@ -27,9 +28,10 @@ public class DatosRangos {
         this.numMax = numMax;
     }
     
-    public synchronized BigInteger repartidor(String tipoRespuesta) {       
+    public synchronized BigInteger repartidor(String tipoRespuesta) {
         BigInteger respuesta = null;
         if ("inicio".equals(tipoRespuesta)){
+            ++contadorHilos;
             if (despertador==0){
                 respuesta = inicioRango;
                 finRango=inicioRango.add(reparto);
@@ -37,6 +39,12 @@ public class DatosRangos {
                 despertador=1;
             }else if(despertador==1){
                 try {
+                //Con la variable despertador si es el segundo hilo que
+                //entra pidiendo el inicio del rango, se queda esperando a
+                //que el primero obtenga el valor del final de su rango y
+                //asi este segundo no obtiene el mismo inicio que el primero,
+                //ya que en el momento que se pide el fin del rango es cuando
+                //se suma al inicio el valor correspondiente del reparto
                     wait();
                     respuesta = inicioRango;
                     finRango=inicioRango.add(reparto);
@@ -47,11 +55,29 @@ public class DatosRangos {
                 }
             }
         }else if ("fin".equals(tipoRespuesta)){
-            respuesta = finRango;
-            despertador=0;
-            notifyAll();
+            if (contadorHilos==numHilos) {
+                respuesta = inicioRango;
+                //Este for comprueba si el ultimo hilo se tiene que
+                //llevar un rango mayor que el resto, por si el reparto
+                //del rango entre los hilos no es exacto
+                int compare=inicioRango.compareTo(finINICIAL);
+                for (BigInteger i=inicioRango; compare==-1; i=i.add(BigInteger.ONE)) {
+                    respuesta = respuesta.add(BigInteger.ONE);
+                    compare=respuesta.compareTo(finINICIAL);
+                }
+            }else{
+                respuesta = finRango;
+                despertador=0;
+                notifyAll();
+            }
         }
         return respuesta;
     }// repartidor()
+    
+    public boolean calculados(BigInteger calculado) {
+        
+        
+        return false;  
+    }// calculados()
     
 }// DatosRangos
--- Teorema 3x+1/src/teorema/pkg3x/pkg1/HiloRangos.java
+++ Teorema 3x+1/src/teorema/pkg3x/pkg1/HiloRangos.java
@@ -13,9 +13,23 @@ public class HiloRangos implements Runnable {
 
     @Override
     public void run(){
+        //Los hilos envian un mensaje de inicio y fin para
+        //controlar que dos hilos no tengan el mismo inicio
         inicioRango=datos.repartidor("inicio");
         finRango=datos.repartidor("fin");
-        System.out.println(""+Thread.currentThread()+inicioRango+"-"+finRango);
+        BigInteger resultado = null;
+        int compare=inicioRango.compareTo(finRango);
+        for (BigInteger i=inicioRango; compare==-1; i=i.add(BigInteger.ONE)) {
+            if (!datos.calculados(i)){
+                if (i.remainder(BigInteger.ONE.add(BigInteger.ONE))==BigInteger.ZERO){
+                    resultado=i.divide(BigInteger.ONE.add(BigInteger.ONE));
+                }else{
+                    resultado=(i.multiply(BigInteger.ONE.add(BigInteger.ONE).add(BigInteger.ONE))).add(BigInteger.ONE);
+                }
+            }
+            System.out.println(""+Thread.currentThread()+inicioRango+"-"+finRango+" "+resultado);
+            compare=i.compareTo(finRango);
+        }
     }// run()
     
 }// HiloRangos
